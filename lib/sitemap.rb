4f03b5f5a3b083752decf0eec1fde8f0ca8e8bb5
Mark Orr <mark.j.l.orr@googlemail.com>
2019-10-26 15:18:50 +0100

    Two sitemaps in one

--- lib/sitemap.rb
+++ lib/sitemap.rb
@@ -1,9 +1,13 @@
-# Helper
-def url(path, date, priority, freq="yearly")
-  url = "https://wanderingdragonschess.club/#{path}"
+# Helpers
+def urls(path, date, priority, freq="yearly")
+  url("", path, date, priority, freq)
+  url("www.", path, date, priority, freq)
+end
+
+def url(dom, path, date, priority, freq)
   puts <<-EOL
   <url>
-    <loc>#{url}</loc>
+    <loc>https://#{dom}wanderingdragonschess.club/#{path}</loc>
     <lastmod>#{date}</lastmod>
     <changefreq>#{freq}</changefreq>
     <priority>#{priority}</priority>
@@ -17,25 +21,25 @@ puts %Q(<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">)
 
 # Home page
 last_blog = Blog.pluck(:updated_at).max.to_date
-url("", last_blog, 1.0, "weekly")
+urls("", last_blog, 1.0, "weekly")
 
 # Blogs, first those with tags and then the others.
-url("blogs", last_blog, 0.9, "weekly")
+urls("blogs", last_blog, 0.9, "weekly")
 Blog.where(draft: false).where.not(tag: nil).each do |b|
-  url("blogs/#{b.tag}", b.updated_at.to_date, 0.9)
+  urls("blogs/#{b.tag}", b.updated_at.to_date, 0.9)
 end
 Blog.where(draft: false).where(tag: nil).each do |b|
-  url("blogs/#{b.id}", b.updated_at.to_date, 0.8)
+  urls("blogs/#{b.id}", b.updated_at.to_date, 0.8)
 end
 
 # Players
-url("players", Player.pluck(:updated_at).max.to_date, 0.9, "monthly")
+urls("players", Player.pluck(:updated_at).max.to_date, 0.9, "monthly")
 Player.all.each do |p|
-  url("players/#{p.id}", p.updated_at.to_date, 0.8)
+  urls("players/#{p.id}", p.updated_at.to_date, 0.8)
 end
 
 # Sign in
-url("signin", "2019-10-26", 0.2)
+urls("signin", "2019-10-26", 0.2)
 
 # Finish XML
 puts %Q(</urlset>)
