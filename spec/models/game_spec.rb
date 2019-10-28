232b5722c378e94d05ac66747c6ada93841ad616
Mark Orr <mark.j.l.orr@googlemail.com>
2019-10-28 15:53:39 +0000

    Another study

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -41,6 +41,7 @@ describe Game do
         zaitsev_karpov:    "Igor Zaitsev - Anatoly Karpov, Masters-Candidate Masters, 196...",
         vaganian_pogonina: "Vaganian,R - Pogonina,N, Snowdrops vs Oldhands, 2011, 1-0",
         b_h_wood:          "White to play and mate in 4",
+        e_allen:           "White to play and win",
       }.each do | name, title |
         expect(Game.create!(pgn: pgn_file("#{name}.pgn")).title).to eq title
       end
