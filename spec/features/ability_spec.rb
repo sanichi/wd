f3c8f03367bd2a27c5209ce539c918233358525f
Mark Orr <mark.j.l.orr@googlemail.com>
2019-10-30 22:54:37 +0000

    Simplify ability feature test

--- spec/features/ability_spec.rb
+++ spec/features/ability_spec.rb
@@ -1,103 +1,67 @@
 require 'rails_helper'
 
 describe Ability do
-  def expect_save_cancel_delete(page)
-    expect(page).to have_button t("save")
-    expect(page).to have_css "a", text: t("cancel")
-    expect(page).to have_css "a", text: t("delete")
+
+  it "guest" do
+    visit home_path
+
+    click_link t("blog.blogs")
+    expect(page).to_not have_css "a", text: t("blog.new")
+
+    # click_link t("game.games")
+    # expect(page).to_not have_css "a", text: t("game.new")
+
+    click_link t("player.players")
+    expect(page).to_not have_css "a", text: t("players.new")
+
+    expect(page).to_not have_css "a", text: t("user.users")
   end
 
-  def expect_no_edit(page)
-    expect(page).to_not have_css "a", text: t("edit")
+  it "member" do
+    login create(:user, roles: ["member"])
+
+    click_link t("blog.blogs")
+    expect(page).to_not have_css "a", text: t("blog.new")
+
+    # click_link t("game.games")
+    # expect(page).to_not have_css "a", text: t("game.new")
+
+    click_link t("player.players")
+    expect(page).to_not have_css "a", text: t("players.new")
+
+    click_link t("user.users")
+    expect(page).to_not have_css "a", text: t("user.new")
   end
 
-  def expect_no_go(path, page)
-    visit path
-    expect(page).to have_css "div.alert.alert-dismissable", text: t("unauthorized.default")
+  it "blogger" do
+    login create(:user, roles: ["blogger"])
+
+    click_link t("blog.blogs")
+    click_link t("blog.new")
+
+    # click_link t("game.games")
+    # click_link t("game.new")
+
+    click_link t("player.players")
+    expect(page).to_not have_css "a", text: t("players.new")
+
+    click_link t("user.users")
+    expect(page).to_not have_css "a", text: t("user.new")
   end
 
-  let!(:blog1) { create(:blog, draft: false, user: blogger1) }
-  let!(:blog2) { create(:blog, draft: false, user: blogger2) }
+  it "admin" do
+    login create(:user, roles: ["admin"])
 
-  let(:admin)    { create(:user, roles: ["admin"]) }
-  let(:blogger1) { create(:user, roles: ["blogger"]) }
-  let(:blogger2) { create(:user, roles: ["blogger"]) }
-  let(:member)   { create(:user, roles: ["member"]) }
+    click_link t("blog.blogs")
+    click_link t("blog.new")
 
-  context "admin" do
-    before(:each) do
-      login admin
-    end
+    click_link t("game.games")
+    click_link t("game.new")
 
-    it "blogs" do
-      [blog1, blog2].each do |blog|
-        click_link t("blog.blogs")
-        click_link blog.title
-        click_link t("edit")
-        expect_save_cancel_delete(page)
-      end
-    end
+    click_link t("player.players")
+    click_link t("player.new")
 
-    it "users" do
-      [admin, blogger1, blogger2].each do |user|
-        click_link t("user.users")
-        click_link user.handle
-        click_link t("edit")
-        expect_save_cancel_delete(page)
-      end
-    end
-  end
-
-  context "blogger" do
-    before(:each) do
-      login blogger1
-    end
-
-    it "blogs" do
-      click_link t("blog.blogs")
-      click_link blog1.title
-      click_link t("edit")
-      expect_save_cancel_delete(page)
-
-      click_link t("blog.blogs")
-      click_link blog2.title
-      expect_no_edit page
-      expect_no_go edit_blog_path(blog2), page
-    end
-
-    it "users" do
-      [blogger1, blogger2].each do |blogger|
-        click_link t("user.users")
-        expect(page).to have_css "td", text: blogger.handle
-        expect(page).to_not have_css "a", text: blogger.handle
-        expect_no_go user_path(blogger), page
-        expect_no_go edit_user_path(blogger), page
-      end
-    end
-  end
-
-  context "member" do
-    before(:each) do
-      login member
-    end
-
-    it "blogs" do
-      [blog1, blog2].each do |blog|
-        click_link t("blog.blogs")
-        click_link blog.title
-        expect_no_edit page
-        expect_no_go edit_blog_path(blog), page
-      end
-    end
-
-    it "users" do
-      [member, blogger1, blogger2].each do |user|
-        click_link t("user.users")
-        expect(page).to have_css "td", text: user.handle
-        expect_no_edit(page)
-        expect_no_go user_path(user), page
-        expect_no_go edit_user_path(user), page
-      end
-    end
+    click_link t("user.users")
+    click_link t("user.new")
   end
 end
