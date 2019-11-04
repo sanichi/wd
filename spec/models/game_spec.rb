f8c744fdb1a76c62b8a1cdca39a8709f5761172c
Mark Orr <mark.j.l.orr@googlemail.com>
2019-11-04 14:52:13 +0000

    Updated test for new file

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,6 +9,7 @@ describe Game do
         vaganian_pogonina: "Vaganian,R - Pogonina,N, Snowdrops vs Oldhands, 2011, 1-0",
         b_h_wood:          "White to play and mate in 4",
         e_allen:           "White to play and win",
+        kasas_debarnot:    "Black to play and win",
       }.each do | name, title |
         expect(Game.create!(pgn: pgn_file("#{name}.pgn")).title).to eq title
       end
