3244ab7fe4b3fbbbf32196deb9b057d839795a70
Mark Orr <mark.j.l.orr@googlemail.com>
2019-12-19 17:57:08 +0000

    Added handy routes checker

---
+++ lib/check_routes.rb
@@ -0,0 +1,36 @@
+# just run with ruby check_routes.rb from route directory
+
+require_relative "../config/environment"
+
+Rails.application.eager_load!
+
+results =
+  Rails.application.routes.routes.map(&:requirements).reject(&:empty?).map do |r|
+    name = r[:controller].camelcase
+    controller = "#{name}Controller"
+    controller_with_action = "#{name}##{r[:action]}"
+
+    if Object.const_defined?(controller)
+      controller_instance = controller.constantize.new
+    else
+      next { controller_with_action => false }
+    end
+
+    next if name.start_with?("Rails")
+
+    if controller_instance.respond_to?(r[:action])
+      { controller_with_action => true }
+    else
+      { controller_with_action => Dir.glob(Rails.root.join("app", "views", name.downcase, "#{r[:action]}.*")).any? }
+    end
+  end
+
+unused_controller_actions = results.reject(&:blank?).reject { |r| r.values.first }.uniq
+
+if unused_controller_actions.empty?
+  puts "#{Rails.application.routes.routes.size} total routes. No unused routes found!"
+else
+  puts "Unused routes found (#{unused_controller_actions.size})"
+
+  unused_controller_actions.map(&:first).map(&:first).each { |name| puts " " + name }
+end
