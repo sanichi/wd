1753a5e03a0ece710db4b4f25ee2b4f000fd80e2
Mark Orr <mark.j.l.orr@googlemail.com>
2021-01-05 11:02:23 +0000

    Gem update including rails 6.1.0

--- Gemfile
+++ Gemfile
@@ -1,31 +1,29 @@
 source 'https://rubygems.org'
 
-gem 'rails', '6.0.3.4'
+gem 'rails', '6.1.0'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'haml-rails', '~> 2.0'
-gem 'sassc-rails', '~> 2.1'
-gem 'jbuilder', '~> 2.7'
-gem 'uglifier', '~> 4.2'
 gem 'jquery-rails', '~> 4.3'
-gem 'meta-tags', '~> 2.12'
-gem 'redcarpet', '~> 3.5'
+gem 'sassc-rails', '~> 2.1'
 gem 'autoprefixer-rails', '~> 10.0'
 gem 'bootstrap', '~> 4.5'
-gem 'bcrypt', '~> 3.1.7'
+gem 'uglifier', '~> 4.2'
+gem 'meta-tags', '~> 2.12'
+gem 'redcarpet', '~> 3.5'
 gem 'cancancan', '~> 3.0'
+gem 'bcrypt', '~> 3.1.7'
 gem 'actionview-encoded_mail_to', '~> 1.0'
 gem 'pgn', '~> 0.3'
 gem 'mechanize', '~> 2.7', require: false
-gem 'bootsnap', '>= 1.4.2', require: false # required in config/boot.rb
+gem 'jbuilder', '~> 2.7'
 
 group :development, :test do
-  gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 4.0'
   gem 'capybara', '~> 3.28'
-  gem 'factory_bot_rails', '~> 6.1'
-  gem 'faker', '~> 2.1'
-  gem 'launchy', '~> 2.4'
-  gem 'awesome_print', require: 'ap'
+  gem 'byebug', platforms: :mri
+  gem 'launchy', '~> 2.5'
+  gem 'factory_bot_rails', '~> 6.0'
+  gem 'faker', '~> 2.10'
 end
 
 group :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -22,25 +24,27 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress CSS using a preprocessor (:sass is default if sass-rails present).
+  # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  # config.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # config.active_storage.service = :local
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # You may want to use the lowest log level (:debug) to ensure availability of diagnostic information
-  # when problems arise.
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
@@ -51,9 +55,9 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sni_wd_app_production"
+  # config.active_job.queue_name_prefix = "app_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -66,11 +70,17 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # Log disallowed deprecations.
+  config.active_support.disallowed_deprecation = :log
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  # require 'syslog/logger'
+  # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
