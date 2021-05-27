29f8559582d1b95600e88ac7e5df621ea76d4915
Mark Orr <mark.j.l.orr@googlemail.com>
2021-05-27 17:14:32 +0100

    Upgrade to bootstrap 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,14 +19,49 @@ module ApplicationHelper
     "alert alert-#{bootstrap_name}"
   end
 
-  def center(xs: 0, sm: 0, md: 0, lg: 0, xl: 0)
+  def center(xs: 0, sm: 0, md: 0, lg: 0, xl: 0, xx: 0)
     klass = []
-    klass.push "offset-#{(12 - xs) / 2} col-#{xs}" if xs > 0
-    klass.push "offset-sm-#{(12 - sm) / 2} col-sm-#{sm}" if sm > 0
-    klass.push "offset-md-#{(12 - md) / 2} col-md-#{md}" if md > 0
-    klass.push "offset-lg-#{(12 - lg) / 2} col-lg-#{lg}" if lg > 0
-    klass.push "offset-xl-#{(12 - xl) / 2} col-xl-#{xl}" if xl > 0
+    klass.push "offset-#{(12 - xs) / 2} col-#{xs}"         if xs > 0
+    klass.push "offset-sm-#{(12 - sm) / 2} col-sm-#{sm}"   if sm > 0
+    klass.push "offset-md-#{(12 - md) / 2} col-md-#{md}"   if md > 0
+    klass.push "offset-lg-#{(12 - lg) / 2} col-lg-#{lg}"   if lg > 0
+    klass.push "offset-xl-#{(12 - xl) / 2} col-xl-#{xl}"   if xl > 0
+    klass.push "offset-xxl-#{(12 - xx) / 2} col-xxl-#{xx}" if xx > 0
     klass.any? ? klass.join(" ") : "col-12"
+  end
+
+  def centre(*args)
+    nums = args.map(&:to_i).select{ |n| n > 0 && n < 12 }.sort.reverse
+    return "col" if nums.empty?
+    nums = nums.first(6) if nums.length > 6
+    nums.unshift(12) while nums.length < 6
+    cols = %w/xs sm md lg xl xxl/
+    0.upto(5).map do |i|
+      if nums[i] < 12
+        if i == 0
+          "col-#{nums[i]}"
+        else
+          "col-#{cols[i]}-#{nums[i]}"
+        end
+      else
+        nil
+      end
+    end.compact.join(" ")
+  end
+
+  def col(s)
+    case s
+    when true
+      "col-12"
+    when false, nil
+      ""
+    else
+      s.to_s.gsub(/(\A| )(sx|ms|dm|gl|lx|lxx)-(\d|1[012])/) do
+        "#{$1}offset-#{$2 == 'sx' ? '' : $2.reverse + '-'}#{$3}"
+      end.gsub(/(\A| )((?:xs|sm|md|lg|xl|xxl)-)?(\d|1[012])/) do
+        "#{$1}col-#{$2 == 'xs-' ? '' : $2}#{$3}"
+      end
+    end
   end
 
   def encrypted_mail_to(email)
