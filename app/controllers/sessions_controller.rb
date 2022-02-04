7daec47acb1fcb6c2590ca2ed18a70f2a23105c2
Mark Orr <mark.j.l.orr@googlemail.com>
2022-02-04 21:59:27 +0000

    Tweak

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(handle: params[:handle])
     user = user&.authenticate(params[:password]) unless current_user.admin?
     if user
-      if user.otp_required?
+      if user.otp_required? && !current_user.admin?
         session[:otp_user_id] = user.id
         redirect_to new_otp_secret_path
       else
