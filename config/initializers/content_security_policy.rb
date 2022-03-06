2cbbccfd31fd0a8ee585dfa113e917d0d2bf42fa
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-06 21:57:59 +0000

    CSP is too hard for this app

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,23 +4,20 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
-    policy.default_src     :none
-#   policy.font_src        :self, :https, :data
-    policy.img_src         :self
-#   policy.object_src      :none
-    policy.script_src      :self
-    policy.style_src       :self, :unsafe_inline
-    policy.frame_ancestors :none
-    policy.form_action     :self
-    policy.report_uri      "https://sanichi.report-uri.com/r/d/csp/wizard"
-  end
-
-  # Generate session nonces for permitted importmap and inline scripts.
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w(script-src)
-
-  # Just report for now.
-  config.content_security_policy_report_only = true
-end
+# Rails.application.configure do
+#   config.content_security_policy do |policy|
+#     policy.default_src     :none
+#     policy.font_src        :self, :https, :data
+#     policy.img_src         :self
+#     policy.object_src      :none
+#     policy.script_src      :self
+#     policy.style_src       :self, :unsafe_inline
+#     policy.frame_ancestors :none
+#     policy.form_action     :self
+#     policy.report_uri      "https://sanichi.report-uri.com/r/d/csp/wizard"
+#   end
+#
+#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+#   config.content_security_policy_nonce_directives = %w(script-src)
+#   config.content_security_policy_report_only = true
+# end
