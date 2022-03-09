6af407db376a507507b83e525e0320c62b4562ac
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-09 17:57:47 +0000

    Experimental CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,20 +4,20 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src     :none
-#     policy.font_src        :self, :https, :data
-#     policy.img_src         :self
-#     policy.object_src      :none
-#     policy.script_src      :self
-#     policy.style_src       :self, :unsafe_inline
-#     policy.frame_ancestors :none
-#     policy.form_action     :self
-#     policy.report_uri      "https://sanichi.report-uri.com/r/d/csp/wizard"
-#   end
-#
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-#   config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.base_uri        :self
+    policy.default_src     :none
+    policy.font_src        :none
+    policy.img_src         :self
+    policy.object_src      :none
+    policy.script_src      :self
+    policy.style_src       :self, :unsafe_inline # redcarpet
+    policy.frame_ancestors :none
+    policy.form_action     :self
+    policy.report_uri      "https://sanichi.report-uri.com/r/d/csp/wizard"
+  end
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w(script-src)
+  config.content_security_policy_report_only = true
+end
