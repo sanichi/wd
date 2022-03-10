7f841854ddc7abf412286c481bc0c85f97d02dad
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-10 20:34:13 +0000

    Tweak

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     policy.base_uri        :self
     policy.default_src     :none
-    policy.img_src         :self
+    policy.img_src         :self, :data
     policy.script_src      :self
     policy.style_src       :self, :unsafe_inline # redcarpet
     policy.frame_ancestors :none
