745bec613aac645d610ca5c6d0445e2cc43e4b63
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-11 10:31:18 +0000

    Policy correction

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,11 +6,12 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    policy.base_uri        :self
     policy.default_src     :none
+    policy.connect_src     :self
     policy.img_src         :self, :data
     policy.script_src      :self
     policy.style_src       :self, :unsafe_inline # redcarpet
+    policy.base_uri        :self
     policy.frame_ancestors :none
     policy.form_action     :self
   end
