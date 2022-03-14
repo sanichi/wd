d0d48fdd325075257111d9efb2844e5c84f59c25
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-14 15:51:44 +0000

    Tweak

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
     policy.base_uri        :self
     policy.frame_ancestors :none
     policy.form_action     :self
+    policy.report_uri      "https://hou.sanichi.me/reports"
   end
   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
   config.content_security_policy_nonce_directives = %w(script-src)
