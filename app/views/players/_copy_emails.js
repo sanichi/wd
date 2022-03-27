c4160bf363cb3dd2b2fbebe1606bf627816df423
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-27 14:17:40 +0100

    Removed execCommand and used clipboard API instead

--- app/views/players/_copy_emails.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('#copy_emails').click(function(e) {
-    e.preventDefault();
-    document.getElementById('emails_to_copy').select();
-    document.execCommand('copy');
-    document.getElementById('role').focus();
-  });
-});
