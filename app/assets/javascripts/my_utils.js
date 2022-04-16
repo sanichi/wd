2d726eaa7004aa7341ab9549cb8c956dfb1e3d62
Mark Orr <mark.j.l.orr@googlemail.com>
2022-04-16 14:36:34 +0100

    Gem update and remote forms fix

--- app/assets/javascripts/my_utils.js
+++ app/assets/javascripts/my_utils.js
@@ -1,6 +1,7 @@
-$(function() {
+$(function () {
   // Auto-submit on change.
-  $('form .auto-submit').change(function() {
-    $(this).parents('form').submit();
+  $('form .auto-submit').change(function () {
+    var form = $(this).parents('form');
+    form.attr('data-remote') == "true" ? Rails.fire(form[0], 'submit') : form.submit();
   });
 });
