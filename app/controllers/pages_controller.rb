71173e9187499654f2bda038b69de605062a0658
Mark Orr <mark.j.l.orr@googlemail.com>
2023-11-23 12:49:24 +0000

    Enhanced env page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   # see https://github.com/CanCanCommunity/cancancan/wiki/Non-RESTful-Controllers
   authorize_resource class: false
 
+  def env
+    dirs = `ls /home/sanichi/.passenger/native_support`
+    vers = dirs.scan(/\d*\.\d*\.\d*/)
+    @passenger_version = vers.any? ? vers.last : "not found"
+    vers = ActiveRecord::Base.connection.execute('select version();').values[0][0] rescue "oops"
+    @postgres_version = vers.match(/(1[4-6]\.\d+)/)? $1 : "not found"
+    @host = ENV["HOSTNAME"] || `hostname`.chop.sub(".local", "")
+  end
+
   def home
     @blogs = Blog.where(draft: false, pin: true).updated_des
     @blogs+= Blog.where(draft: false, pin: false).created_des.limit(5)
