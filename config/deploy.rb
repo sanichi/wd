caff5c5eb7ccf5dd8d692c3e030e11c4649a1491
Mark Orr <mark.j.l.orr@googlemail.com>
2025-07-27 11:16:11 +0100

    Take public/img/photos out of git and handle with rsync instead

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "wd"
 set :repo_url, "git@github.com:sanichi/wd.git"
 append :linked_files, "config/database.yml", "config/master.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/img/photos"
 set :log_level, :info
 set :branch, "main"
 
